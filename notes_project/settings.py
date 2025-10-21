d3b667b5a158f0660bec298ca4409c87f6ecf60f
SEBASTIAN LAGUNA FUNES <75394396+sebaslagu@users.noreply.github.com>
2025-10-20 23:47:13 -0500

    Add initial project files

---
+++ notes_project/settings.py
@@ -0,0 +1,13 @@
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'django.contrib.contenttypes', 'django.contrib.sessions', 'django.contrib.messages', 'django.contrib.staticfiles', 'notes.apps.NotesConfig']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+LANGUAGE_CODE = 'es'
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
